acde7771f90b9c08cc3f44528914e6a09a728395
KUI <www.wjan531202804@qq.com>
2022-09-06 11:14:39 +0800

    feat:项目初始化

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
 const { defineConfig } = require('@vue/cli-service')
+const { resolve } = require('path')
+const com = resolve(__dirname, 'src/components')
+const views = resolve(__dirname, 'src/views')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 配置开发服务器，路径别名
+  configureWebpack: {
+    // 所写的所有的东西和webpack的配置一样
+    devServer: {
+      open: true, // 打开服务器的时候自动打开浏览器
+      port: 9995, // 指定域名到本机
+      host: 'localhost'
+    },
+    resolve: {
+      alias: {
+        com,
+        views
+      }
+    }
+  }
 })
